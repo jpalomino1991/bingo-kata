e6e7a58dead2cefb6367fbfc4d8f1b877383c7de
jpalomino1991 <palomino.j@pucp.edu.pe>
2020-06-03 12:05:46 -0500

    change route name

--- resources/player/player.router.js
+++ resources/player/player.router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const controllers = require('./player.controller');
 const router = express.Router();
 
-router.get('/generateCard', controllers.checkPlayerCard)
+router.get('/checkplayer', controllers.checkPlayerCard)
 
 module.exports = router;
